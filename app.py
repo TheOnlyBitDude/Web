60ef9962fd8cb4df87016742311b05f327b68e11
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-11-27 21:12:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title="A.")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 9128))
